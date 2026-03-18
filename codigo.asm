136d69ea882e9e58e4f798fff8001da2e1605c4a
kioshiotani <brunokioshiotani@gmail.com>
2026-03-18 15:42:36 -0300

    Implementado parcialmente funcao insert_front

--- codigo.asm
+++ codigo.asm
@@ -17,37 +17,57 @@ main:
 # -----------------------------------------------
 # create_wagon
 # argumentos:
-#	- a1 : ID
-#	- a2 : Tipo
-#	- a3 : Prox vagao
+#	- a0 : ID
+#	- a1 : Tipo
+#	- a2 : Prox vagao
 # retorno:
 #	- a0 : Endereco do no' do vagao
 # -----------------------------------------------
+create_wagon:	# Salvamento dos argumentos
+		mv t1, a0
+		mv t2, a1
+		mv t3, a2
 
-create_wagon:	# Alocacao de espaco
+		# Alocacao de espaco
 		li a7, 9 # codigo de sbrk
 		li a0, 12 # 12 bytes = 4 bytes (ID=Int) + 4 bytes (Tipo=Int) + 4 bytes (End. do prox. no')
 		ecall # a0 = endereco da area reservada
 	
 		# Armazenamento do ID
-		sw a1, 0(a0)
+		sw t1, 0(a0)
 		
 		# Armazenamento do tipo
-		sw a2, 4(a0)
+		sw t2, 4(a0)
 		
 		# Armazenamento do endereco do prox vagao
-		sw a3, 8(a0)
+		sw t3, 8(a0)
 		
 		jr ra
 
 # -----------------------------------------------
 # insert_front
 # argumentos:
-#	- a1 : endereco da locomotiva
-#	- a2 : ID
-#	- a3 : Tipo
+#	- a0 : endereco da locomotiva
+#	- a1 : ID
+#	- a2 : Tipo
 # -----------------------------------------------
+insert_front:	# Salvamento dos argumentos
+		mv t1, a0
+		mv t2, a1
+		mv t3, a2
+		
+		# Salvamento do endereco de retorno
+		# ...
 
-
+		# Criar vagao
+		mv a0, t1
+		mv a1, t2
+		lw a2, 8(t1) # carregar como argumento do novo vagao o vagao que vinha logo apos a locomotiva		
+		jal ra, create_wagon # < --- Problema
+		
+		# Atualizar prox endereco da locomotiva
+		sw a0, 8(t1)
+		
+		jr ra # < --- Problema
 		
 				
